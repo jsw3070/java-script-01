e237a3e9d01aa6915ccd4ecff0c24e553d41fa5b
SangWon Jung <sangwon.jung@one-line.com>
2019-02-07 10:55:29 +0900

    index4.html

---
+++ js/index3.js
@@ -0,0 +1,21 @@
+const log = console.log;
+
+//반복문
+// for(초기값; 조건; 증가값) {
+
+$("#bt-ok").click(function(){
+ for(var i=1; i<=100; i++) {
+    $("#box-wrap").append('<div>'+i+'</div>');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
